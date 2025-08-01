549c4d9263b7cfa747cbd2dfaff5fc03a1d5b3f0
William Levy <levy.william.j@gmail.com>
2025-08-01 14:21:23 -0400

    Created basic layout for the app

--- TodoConsoleApp/Program.cs
+++ TodoConsoleApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string userInput;
+List<string> todoList = new List<string>();
+
+header();
+showAllTodos();
+userOptionMenu();
+
+
+
+void header()
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?\n");
+}
+
+void showAllTodos()
+{
+    if (todoList.Count == 0)
+    {
+        Console.WriteLine("TODO List is empty.\n");
+    }
+    else
+    {
+        Console.WriteLine("This will display all todos");
+    }
+
+}
+
+void userOptionMenu()
+{
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[U]pdate a TODOs");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+}
+
+Console.ReadKey();
